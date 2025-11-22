ce297ba33d85de3f73d848a9e94497a17cead7ae
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2025-11-22 18:53:23 +0100

    complex sample

--- src/app/config-tool/config-tool.ts
+++ src/app/config-tool/config-tool.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { ConfigViewer } from "../config-viewer/config-viewer";
 
 @Component({
@@ -6,13 +6,76 @@ import { ConfigViewer } from "../config-viewer/config-viewer";
   imports: [ConfigViewer],
   template: `
     <div>
-      <h1>Configuration Tool</h1>
+      <label for="show-advanced">Show Advanced:</label>
+      <input
+        type="checkbox" id="show-advanced"
+        [checked]="showAdvanced()"
+        (change)="showAdvanced.set($event.target.checked)" />
     </div>
 
-    <app-config-viewer></app-config-viewer>
+
+    <div>
+      <label for="theme-select">Theme:</label>
+      <select
+        id="theme-select"
+        [value]="theme()"
+        (change)="handleThemeChange($event)">
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </div>
+
+    <div>
+    <label for="user-role">User role</label>
+    <input
+      type="text"
+      id="user-role"
+      [value]="userRole()"
+      (change)="userRole.set($event.target.value)"
+      />
+    </div>
+
+    <div>
+    <label for="region">Region</label>
+    <input
+      type="text"
+      id="region"
+      [value]="region()"
+      (change)="region.set($event.target.value)"
+      />
+    </div>
+    <div>
+    <label for="features">Feature flags</label>
+    <input
+      type="text"
+      id="features"
+      [value]="features()"
+      (change)="features.set($event.target.value)"
+      />
+    </div>
+
+
+    <app-config-viewer
+      [userRole]="userRole()"
+      [theme]="theme()"
+      [showAdvanced]="showAdvanced()"
+      [region]="region()"
+      [features]="features()">
+    </app-config-viewer>
   `,
   styles: ``,
 })
 export class ConfigTool {
+  showAdvanced = signal(false);
+  theme = signal<string>('light');
+  userRole = signal<string>('guest');
+  region = signal<string>('us-east-1');
+  features = signal<string>('');
+
+  handleThemeChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.theme.set(selectElement.value as 'light' | 'dark');
+  }
+
 
 }
--- src/app/config-viewer/config-viewer.ts
+++ src/app/config-viewer/config-viewer.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-config-viewer',
   imports: [],
   template: `
-    <p>
-      config-viewer works!
-    </p>
+    <h3>Config panel</h3>
+    <ul>
+      <li>Is admin: {{ isAdmin() }}</li>
+      <li>Is Dark mode: {{isDarkMode()}}</li>
+      <li>Experimental: {{experimental()}} (is admin and show advanced)</li>
+      <li>Region supports featureX: {{featureXSupported()}} (featureX present and region is EU))</li>
+      <li>Legal banner showed: {{legalBannerVisible()}} (region is EU, not admin, and no suppress-legal feature)</li>
+    </ul>
+
   `,
   styles: ``,
 })
 export class ConfigViewer {
+  userRole = input<string>();
+  theme = input<string>();
+  showAdvanced = input<boolean>();
+  region = input<string>();
+  features = input<string>();
+
+  isAdmin = computed(
+    () => this.userRole() === 'admin'
+  );
+
+  isDarkMode = computed(
+    () => this.theme() === 'dark'
+  );
+
+  experimental = computed(
+    () => this.isAdmin()
+       && this.showAdvanced()
+  );
+
+  featureXSupported = computed(
+    () => this.region() === 'EU'
+       && this.features()?.includes('featureX')
+  );
+
+  legalBannerVisible = computed(
+    () => this.region() === 'EU'
+       && !this.isAdmin()
+       && !this.features()?.includes('suppress-legal')
+  );
 
 }
