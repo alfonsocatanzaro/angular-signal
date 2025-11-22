37d76669e31d4550b033a6f41399029794f46c07
Alfonso Catanzaro <alfonso.catanzaro@gmail.com>
2025-11-22 16:53:42 +0100

    Move outside list of links

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+interface NavItem {
+  title: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +14,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('angular-signal');
+  protected readonly navItems: NavItem[] = [
+    { title: 'Simple counter', link: '/simple-counter' },
+    { title: 'LinkedSignal', link: '/linked-signal-container' },
+    { title: 'Debouncing', link: '/debouncing' },
+    { title: 'Conditional Rendering', link: '/conditional-rendering' },
+    { title: 'HTTP resource', link: '/http-resource' },
+    { title: 'Config Tool', link: '/config-tool' },
+  ];
 }
